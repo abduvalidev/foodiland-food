55f41b792766ab3159768791281203cd4402b54c
unknown <abduvalidev1@gmail.com>
2024-08-31 23:35:39 +0500

    add to modal video

--- src/components/Home/Recipes.jsx
+++ src/components/Home/Recipes.jsx
@@ -91,48 +91,46 @@ export default function Recipes() {
 
   return (
     <section id="recipe" className="mt-8 md:mt-16 lg:mt-32 xl:mt-44">
-      <div className="w-container">
-        <div className="text-center">
-          <h2>Simple and tasty recipes</h2>
-          <p className="mt-4 md:mt-6">
-            Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqut enim ad minim{" "}
-          </p>
-        </div>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-8 lg:gap-10 mt-8 md:mt-16 lg:mt-24">
-          {cards.map((card, index) => (
-            <div
-              key={index}
-              className="bg-gradient-to-b from-brand-gray-200/0 to-brand-gray-200/100 relative rounded-[30px] p-4 space-y-4 md:space-y-6 group transition duration-300 hover:shadow-lg"
-            >
-              <LikeButton />
-              <div className="aspect-video md:aspect-[368/250] overflow-hidden rounded-2xl">
-                <img
-                  className="w-full h-full object-cover transition duration-300 group-hover:scale-110"
-                  src={card.img}
-                  alt="img"
-                />
+      <div className="text-center">
+        <h2>Simple and tasty recipes</h2>
+        <p className="mt-4 md:mt-6">
+          Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqut enim ad minim{" "}
+        </p>
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-8 lg:gap-10 mt-8 md:mt-16 lg:mt-24">
+        {cards.map((card, index) => (
+          <div
+            key={index}
+            className="bg-gradient-to-b from-brand-gray-200/0 to-brand-gray-200/100 relative rounded-[30px] p-4 space-y-4 md:space-y-6 group transition duration-300 hover:shadow-lg"
+          >
+            <LikeButton />
+            <div className="aspect-video md:aspect-[368/250] overflow-hidden rounded-2xl">
+              <img
+                className="w-full h-full object-cover transition duration-300 group-hover:scale-110 select-none"
+                src={card.img}
+                alt="img"
+              />
+            </div>
+            <h4 className="font-semibold text-lg md:text-2xl px-2">
+              {card.title}
+            </h4>
+            <div className="flex items-center gap-x-4 lg:gap-x-6 pb-4 px-2">
+              <div className="flex items-center gap-x-2.5">
+                <img src={TimerSvg} alt="time" />
+                <span className="font-medium text-black/60 text-sm">
+                  30 Minutes
+                </span>
               </div>
-              <h4 className="font-semibold text-lg md:text-2xl px-2">
-                {card.title}
-              </h4>
-              <div className="flex items-center gap-x-4 lg:gap-x-6 pb-4 px-2">
-                <div className="flex items-center gap-x-2.5">
-                  <img src={TimerSvg} alt="time" />
-                  <span className="font-medium text-black/60 text-sm">
-                    30 Minutes
-                  </span>
-                </div>
-                <div className="flex items-center gap-x-2.5">
-                  <img src={ForkKnife} alt="fork" />
-                  <span className="font-medium text-black/60 text-sm">
-                    {card.dishName}
-                  </span>
-                </div>
+              <div className="flex items-center gap-x-2.5">
+                <img src={ForkKnife} alt="fork" />
+                <span className="font-medium text-black/60 text-sm">
+                  {card.dishName}
+                </span>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </section>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,18 @@ import Hero from "../components/Home/Hero";
 import Categories from "../components/Home/Categories";
 import Recipes from "../components/Home/Recipes";
 import Cooker from "../components/Home/Cooker";
+import CheckOut from "../components/Home/CheckOut";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <div className="w-container">
       <Categories />
       <Recipes />
       <Cooker />
+      <CheckOut />
+      </div>
     </div>
   );
 }
